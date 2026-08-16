6653d71e1f86da402da9dcbab477eaa1835cf304
Gutemberg <gutemberg.guieiro@gmail.com>
2026-08-16 20:32:09 -0300

    Create desafio-04.sql

---
+++ 03-Consultas/desafio-04.sql
@@ -0,0 +1,8 @@
+--DESAFIO 04
+
+--OBJETIVO: Listar todos os produtos ordenados pelo preço do maior para o menor.
+
+--Conceitos utilizados: SELECT, FROM e ORDER BY.
+
+SELECT * FROM produtos
+ORDER BY preco DESC;
