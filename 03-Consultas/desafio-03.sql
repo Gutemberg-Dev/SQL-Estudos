0a6f987df6a9e5ad856296bf24d1c51c76faa070
Gutemberg <gutemberg.guieiro@gmail.com>
2026-08-16 20:32:06 -0300

    Create desafio-03.sql

---
+++ 03-Consultas/desafio-03.sql
@@ -0,0 +1,8 @@
+--DESAFIO 03
+
+--OBJETIVO: Mostrar apenas os clientes que moram em Belo Horizonte.
+
+--Conceitos utilizados: SELECT, FROM e WHERE.
+
+SELECT * FROM clientes
+ WHERE cidade = 'Belo Horizonte';
